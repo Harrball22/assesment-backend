85c32db4721430547e35eaef7abe8ebf3ea22f9a
Harrison Dutson <totodutson@gmail.com>
2023-05-05 13:51:25 -0600

    Added an add fortune form. Functional

--- client/main.js
+++ client/main.js
@@ -1,5 +1,7 @@
 const complimentBtn = document.getElementById("complimentButton")
 const fortuneButton = document.getElementById("fortune-button")
+const addFortuneForm = document.getElementById("add-fortune-form")
+const addFortuneText = document.getElementById("add-fortune-text")
 
 const getCompliment = () => {
     axios.get("http://localhost:4000/api/compliment/")
@@ -17,5 +19,19 @@ const getFortune = () => {
     })
 }
 
+const addFortune = (e) => {
+    e.preventDefault()
+    const newFortune = addFortuneText.value
+
+    axios.post("http://localhost:4000/api/fortune/", {fortune: newFortune})
+    .then(res => {
+        alert(res.data)
+    })
+    .catch(err => {
+        console.log(err)
+    })
+}
+
 complimentBtn.addEventListener('click', getCompliment)
 fortuneButton.addEventListener("click", getFortune)
+addFortuneForm.addEventListener("submit", addFortune)
